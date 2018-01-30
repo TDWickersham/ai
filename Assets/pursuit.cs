68f3b952bdcec23dd27b079e6f7a730ea9733d59
Caleb Seay <s179067@students.aie.edu.au>
2018-01-29 17:15:30 -0800

    ai coding

---
+++ Assets/pursuit.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pursuit : MonoBehaviour {
+
+    Rigidbody rb;
+    Rigidbody targetRb;
+    Vector3 desiredVelocity;
+    Vector3 projectedPosition;
+    public float speed;
+    public Transform target;
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+        targetRb = target.GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        projectedPosition = target.position + targetRb.velocity;
+        desiredVelocity = speed * (projectedPosition - transform.position).normalized;
+        rb.AddForce(desiredVelocity - rb.velocity);
+	}
+}
---
+++ Assets/seeker.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class seeker : MonoBehaviour {
+
+
+    Rigidbody rb; 			// The Rigidbody on which to apply force to
+
+    Vector3 desiredVelocity; 		// the desired direction and speed in which to move
+
+    public float speed; 			// How fast we will go
+
+    public GameObject Target;       //What object are we moving towards?
+
+    void Start()
+    {
+
+        rb = GetComponent<Rigidbody>();
+        // assigning the Rigidbody we are adding force to, 
+        //  to the rigidbody attached to this object
+    }
+
+    void Update()
+    {
+        desiredVelocity = speed * (Target.transform.position - transform.position).normalized;
+        //Calculating the directional normalized vector
+        // between OUR position and the TARGETs position
+        //Then multiplying it by our speed
+
+
+        rb.AddForce(desiredVelocity - rb.velocity);
+        //Subtracting our velocity from our force
+        // provides us with a smoother acceleration
+        //and maintainment of our speed
+
+    }
+}
